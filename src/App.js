5037315bb7fa8cb6ae26ee271541c81f5bd8691a
Haresh M Retrita <haresh.mangtaniretrita@natwest.com>
2024-10-16 13:01:17 +0100

    Added basic UI layout and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React, { useState } from 'react';
 
 function App() {
+const [city, setCity] = useState('');
+
+const handleSearch = (e) => {
+  setCity(e.taget.value);
+}
+
+const searchWeather = () => {
+//API CALLS
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <h1>Weather App</h1>
+      <input
+      type="text"
+      placeholder='Enter city'
+      value={city}
+      onChange={handleSearch}
+      />
+      <button onClick={searchWeather}>Search</button>
+
+      <div className='"weather-info'>
+        {/*Weather data to be displayed here*/}
+      </div>
     </div>
   );
 }
